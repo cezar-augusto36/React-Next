8a156022bbc236158f56a366ab16229141a2d826
cezarograde <cezarograde36@gmail.com>
2021-09-23 14:59:18 -0400

    componentes

---
+++ pages/basico/usandoTitulo.jsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+
+
+export default function usandoTitulo(){
+    
+
+    return(
+        <div>
+            <Titulo  
+            principal = "Pagina de Cadastro"
+            secundario = "Incluir, alterar e excluir coisas!"
+            />
+             <Titulo  
+            principal = "Pagina de Login"
+            secundario = "Informe seu e-mail e senha:"
+            pequeno={true}
+            />
+             
+        </div>
+    )
+}
