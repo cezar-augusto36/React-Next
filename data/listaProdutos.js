efdf0e87c9faebbef80a5cb33473a475b821b6b0
Cézar Augusto <cezarograde@gmaile.com>
2021-11-26 16:09:41 -0400

    repeticao

---
+++ data/listaProdutos.js
@@ -0,0 +1,8 @@
+import Produto from "../model/produto";
+
+export default [
+    new Produto(1,"Ipad",7543),
+    new Produto(2,"Monitor 24",949),
+    new Produto(3,"Computador Gamer",3453),
+    new Produto(4,"Samsung S20+",1927),
+]
